00e8155210f0d395bd747766e31354086fbb564f
704245786yu <704245786@qq.com>
2015-07-01 18:15:15 +0800

    modified echo_epollserv.c

--- echo_epollserv.c
+++ echo_epollserv.c
@@ -5,17 +5,18 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/epoll.h>
+#include <fcntl.h>
+#include "Socket.h"
 
 #define BUF_SIZE 100
 #define EPOLL_SIZE 50
-#define SERV_PORT 8000
-void error_handling(char *buf);
+#define SERV_PORT 8080
+
+void clr_fl(int fd, int flags);
 
 int main(void)
 {
 	int serv_sock, clnt_sock;
-	struct sockaddr_in serv_adr, clnt_adr;
-	socklen_t adr_sz;
 	int str_len, i;
 	char buf[BUF_SIZE];
 
@@ -23,18 +24,10 @@ int main(void)
 	struct epoll_event event;
 	int epfd, event_cnt;
 
-	serv_sock = socket(AF_INET, SOCK_STREAM, 0);
-	memset(&serv_adr, 0, sizeof(serv_adr));
-	serv_adr.sin_family = AF_INET;
-	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serv_adr.sin_port = htons(SERV_PORT);
+	if( (serv_sock = initServSock(SERV_PORT)) == -1)
+		return -1;
 
-	if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1)
-		error_handling("bind() error");
-	if(listen(serv_sock, 5) == -1)
-		error_handling("listen() error");
-
-	epfd = epoll_create(EPOLL_SIZE);
+	epfd = epoll_create1(0);
 	ep_events = (struct epoll_event*)malloc( sizeof(struct epoll_event) * EPOLL_SIZE );
 
 	event.events = EPOLLIN;
@@ -50,8 +43,7 @@ int main(void)
 
 		for(i=0; i<event_cnt; i++){
 			if(ep_events[i].data.fd == serv_sock){
-				adr_sz = sizeof(clnt_adr);
-				clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr, &adr_sz);
+				clnt_sock=accept(serv_sock, NULL, NULL);
 				event.events = EPOLLIN;
 				event.data.fd = clnt_sock;
 				epoll_ctl(epfd, EPOLL_CTL_ADD, clnt_sock, &event);
@@ -73,9 +65,14 @@ int main(void)
 	return 0;
 }
 
-void error_handling(char *buf)
+void clr_fl(int fd, int flags)
 {
-	fputs(buf, stderr);
-	fputs('\n',stderr);
-	exit(1);
+	int flVal;
+	if( (flVal = fcntl(fd, F_GETFL, 0)) == -1 )
+		perror("fcntl F_GETFL error");
+
+	flVal &= ~flags; //turn flags off
+
+	if( fcntl(fd, F_SETFL, flVal) == -1 )
+		perror("fcntl F_SETFL error");
 }
